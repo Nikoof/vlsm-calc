770c7aac62d1a6ad4aad53736bfb46f9bf14d8b8
Nicolas Bratoveanu <nicolasbratoveanu@gmail.com>
2023-07-10 00:27:43 +0300

    Working flake.nix

--- flake.nix
+++ flake.nix
@@ -15,14 +15,53 @@
         };
 
         naersk' = pkgs.callPackage naersk {};
+        libPath = with pkgs; lib.makeLibraryPath [
+            vulkan-loader
+            libGL 
+            libxkbcommon
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+        ];
 
-      in rec {
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
+      in {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            cmake
+            pkg-config
+            fontconfig
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            rustPackages.clippy
+          ];
+
+          LD_LIBRARY_PATH = libPath;
         };
 
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo cmake xorg.libX11 ];
+        defaultPackage = with pkgs; naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = [
+            makeWrapper
+          ];
+
+          buildInputs = [
+            cmake
+            pkg-config
+            fontconfig
+          ];
+
+          postInstall = ''
+            wrapProgram "$out/bin/vlsm-calc" --prefix LD_LIBRARY_PATH : "${libPath}"
+          '';
+
+          LD_LIBRARY_PATH = libPath;
+        };
+
+        defaultApp = flake-utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
         };
       }
     );
